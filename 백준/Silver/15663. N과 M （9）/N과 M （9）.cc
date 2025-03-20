a97f5d3a150bd7c557e5503d8cd150c35f59e22a
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 22:34:11 +0900

    [Silver II] Title: N과 M (9), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::ios;
+using std::string;
+using std::to_string;
+using std::sort;
+using std::vector;
+
+void NM(int NArray[], string result, int start, int length, int N, int M){
+    static int check[10]={};
+    if(length >= M){
+        cout << result << "\n";
+    }else{
+        for(int i = 1; i <= N; i++){
+            if(check[i] != 1 && (NArray[i-1] != NArray[i] || check[i-1] == 1)){
+                check[i] = 1;
+                NM(NArray, result+to_string(NArray[i])+" ", i+1, length+1, N, M);
+                check[i] = 0;
+            }
+           
+        }
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N = 0, M = 0;
+    cin >> N >> M;
+    
+    int NArray[N+1]={};
+    for(int i = 1; i <= N; i++){
+        cin >> NArray[i];
+    }
+    sort(NArray, NArray+N+1);
+    
+    NM(NArray, "", 1, 0, N, M);
+    
+   return 0;
+}
