e127e3a26c2117a38d59266c4ccbb3cbe7a2763f
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 22:34:24 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using std::cout;
+using std::cin;
+using std::ios;
+using std::string;
+using std::to_string;
+using std::sort;
+
+void NM(int NArray[], int N, int M, int start, int count, string result){
+    if(count >= M){
+        cout << result << "\n";
+        return;
+    }else{
+        for(int i = start; i <= N; i++){
+            if(NArray[i] != NArray[i-1]){
+                NM(NArray, N, M, i, count+1, result+to_string(NArray[i])+" ");
+            }
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N = 0, M = 0;
+    cin >> N >> M;
+    
+    int NArray[N+1]={};
+    for(int i = 1; i <= N; i++){
+        cin >> NArray[i];
+    }
+    sort(NArray, NArray+N+1);
+    
+    NM(NArray, N, M, 1, 0, "");
+    
+    return 0;
+}
