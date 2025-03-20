7b155fd2f240401862e89e622fe61cc42c0cbd78
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 16:50:47 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 56 ms, Memory: 5844 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using std::cout;
+using std::cin;
+using std::ios;
+using std::sort;
+
+class Point{
+	public:
+	int x;
+	int y;
+	
+	Point(int x, int y){
+		this->x = x;
+		this->y = y;
+	}
+};
+
+bool Compare(Point* first, Point* second){
+	if(first->y != second->y){
+		return first->y < second->y;
+	}else{
+		return first->x < second->x;
+	}
+}
+
+int main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N = 0;
+	cin >> N;
+	
+	Point * pointArray[N];
+	int x = 0;
+	int y = 0;
+	for(int i = 0; i < N; i++){
+		cin >> x;
+		cin >> y;
+		pointArray[i] = new Point(x, y);
+	}
+	
+	sort(pointArray, pointArray+N, Compare);
+	
+	for(int i = 0; i < N; i++){
+		cout << pointArray[i]->x << " " << pointArray[i]->y << "\n";
+	}
+}
