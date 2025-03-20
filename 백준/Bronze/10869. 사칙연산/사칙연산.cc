860d3a6d39cffe5592f6b9db5909cb055828c0ff
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 16:37:44 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1 = 0, num2 = 0;
+
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+
+	printf("%d\n%d\n%d\n%d\n%d\n", num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 % num2);
+
+	return 0;
+}
