0f99ff1f3973db7576ccdd277ff1737d96e7f05c
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 16:39:03 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1 = 0, num2 = 0, T = 0;
+
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d", &num1);
+		scanf("%d", &num2);
+
+		printf("Case #%d: %d + %d = %d\n", i + 1, num1, num2, num1 + num2);
+	}
+
+	return 0;
+}
