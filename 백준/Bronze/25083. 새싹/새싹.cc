7a03276129b259294822acd5ae654ce092f4f798
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 16:39:29 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(void){
+    cout<<"         ,r'\"7"<<endl;
+    cout<<"r`-_   ,'  ,/"<<endl;
+    cout<<" \\. \". L_r'"<<endl;
+    cout<<"   `~\\/"<<endl;
+    cout<<"      |"<<endl;
+    cout<<"      |"<<endl;
+    
+    return 0;
+}
