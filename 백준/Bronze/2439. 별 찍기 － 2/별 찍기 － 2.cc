7dd458bc3ae705ecba29ef01a7da3435a01bf907
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-03-20 16:40:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1 = 0;
+
+	scanf("%d", &num1);
+
+	for (int i = 0; i < num1; i++)
+	{
+		for (int j = 0; j < num1 - i - 1; j++)
+			printf(" ");
+
+		for (int j = 0; j < i + 1; j++)
+			printf("*");
+		printf("\n");
+	}
+		
+	return 0;
+}
