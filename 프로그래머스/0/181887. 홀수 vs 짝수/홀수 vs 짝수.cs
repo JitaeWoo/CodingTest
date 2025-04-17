be24452995d14cca01504dc9e31b13d59eb63d18
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-17 12:26:59 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int even = 0;
+        int odd = 0;
+        
+        for(int i = 0; i < num_list.Length; i++)
+        {
+            if(i % 2 == 0)
+            {
+                even += num_list[i];
+            }
+            else
+            {
+                odd += num_list[i];
+            }
+        }
+        
+        return odd > even ? odd : even;
+    }
+}
