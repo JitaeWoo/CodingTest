cb37e29842c58d79a3fefc55f9773b3cccdd661e
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-27 14:42:32 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.68 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(string[] intStrs, int k, int s, int l) {
+        List<int> answer = new List<int>();
+        
+        foreach(string str in intStrs)
+        {
+            int num = int.Parse(str.Substring(s, l));
+            
+            if(num > k)
+            {
+                answer.Add(num);
+            }
+        }
+        
+        return answer.ToArray();
+    }
+}
