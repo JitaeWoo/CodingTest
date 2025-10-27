ef37a99d6424ee68298e4813c0bab28562caba5f
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-27 12:44:25 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 159.70 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public string[] solution(string myString) {
+        string[] answer = myString.Split('x', StringSplitOptions.RemoveEmptyEntries);
+        
+        
+        
+        Array.Sort(answer);
+        
+        return answer;
+    }
+}
