37a6ea519a7b97f3c14e1ba8b71e521e27e1c9aa
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-27 12:51:41 +0900

    [level 0] Title: 문자열 섞기, Time: 0.55 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string str1, string str2) {
+        string answer = "";
+        
+        for(int i = 0 ; i < str1.Length; i++)
+        {
+            answer = $"{answer}{str1[i]}{str2[i]}";
+        }
+        
+        return answer;
+    }
+}
