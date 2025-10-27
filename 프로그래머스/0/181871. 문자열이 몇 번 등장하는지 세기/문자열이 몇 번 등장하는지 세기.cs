6d5f8ed9be8c790ec0598b952fadcda43d3285cf
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-27 15:15:26 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.38 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(string myString, string pat) {
+        int answer = 0;
+        
+        for(int i = 0; i < myString.Length - pat.Length + 1; i++)
+        {
+            if(myString.Substring(i, pat.Length) == pat)
+            {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
