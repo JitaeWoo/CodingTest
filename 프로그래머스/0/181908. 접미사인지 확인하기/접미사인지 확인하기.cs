df33af8a16e51022cdd7e7b7cf3ac473abd36037
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-27 12:38:15 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.29 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string is_suffix) {
+        for(int i = 0; i < my_string.Length; i++)
+        {
+            if(my_string.Substring(i) == is_suffix)
+            {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
