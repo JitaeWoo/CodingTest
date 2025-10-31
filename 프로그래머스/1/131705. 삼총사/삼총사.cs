44409b3f75cd5afb0ae2eab6a075a1de7f1218c6
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-31 17:52:41 +0900

    [level 1] Title: 삼총사, Time: 0.28 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        for(int i = 0; i < number.Length; i++)
+        {
+            for(int j = i; j < number.Length; j++)
+            {
+                if(i == j) continue;
+                for(int k = j; k < number.Length; k++)
+                {
+                    if(i == k || j == k) continue;
+                    
+                    if(number[i] + number[j] + number[k] == 0)
+                    {
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
